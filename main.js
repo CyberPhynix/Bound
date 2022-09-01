55edd5f2addfc65093c1f5cc9b4b5d6adca52355
Josh S <spajos07@gmail.com>
2022-09-01 23:25:36 +0200

    Render v0.2

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-const render = require("./render");
-const { tickspeed } = require("./settings/settings.json")
+// import Render from "render.js";
+const { tickspeed } = require("./settings/settings.json");
+const classRender = require("./render");
+
+let Render = new classRender();
+
+// Render.draw(10, 10, 3);
+// Render.draw(10, 15, 3);
+
+// Render.draw(10, 10, 3);
+// Render.draw(28, 7, 3);
+
+// Render.drawLn(10, 10, 28, 7, 6);
+// Render.drawLn(5, 5, 10, 10, 6);
+// Render.drawLn(10, 5, 10, 10, 6);
+// Render.drawRect(10, 5, 20, 10, 4);
+// Render.drawRect(15, 7, 25, 12, 5);
+
+let dl = Render.drawLn;
+
+dl();
 
 function tick() {
-    render()
+    Render.render();
 }
 
-setInterval(tick, 1000 / tickspeed)
-
+setInterval(tick, 1000 / tickspeed);
