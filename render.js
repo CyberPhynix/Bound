52f2c2a7d362893e301414992da9daa16621a8cc
Josh S <spajos07@gmail.com>
2022-09-01 23:26:07 +0200

    Render v0.2

--- render.js
+++ render.js
@@ -1,28 +1,80 @@
-const { resulution, design } = require("./settings/settings.json");
+const { resulution, design, message } = require("./settings/settings.json");
 
-module.exports = function () {
-    console.clear();
+class Render {
+    displayMap;
 
-    displayMap = [];
+    constructor() {
+        this.displayMap = [];
+        for (let i = 0; i < resulution.h; i++) {
+            this.displayMap.push([]);
+        }
 
-    for (let i = 0; i < resulution.h; i++) {
-        displayMap.push([]);
-    }
-
-    for (let i = 0; i < resulution.h; i++) {
-        for (let j = 0; j < resulution.w; j++) {
-            displayMap[i].push(design.bg);
+        for (let i = 0; i < resulution.h; i++) {
+            for (let j = 0; j < resulution.w; j++) {
+                this.displayMap[i].push(design.bg);
+            }
         }
     }
 
-    displayString = "";
+    draw(x, y, type = 1) {
+        x--;
+        y--;
+        this.displayMap[y][x] =
+            design[type] == undefined ? design[1] : design[type];
+    }
 
-    displayMap.forEach((e) => {
-        e.forEach((e) => {
-            displayString += e;
+    drawLn(x1, y1, x2, y2, type = 1) {
+        let calc = (x2 - x1) / (y2 - y1);
+        let distX = x2 - x1;
+        let distY = y2 - y1;
+
+        if (distX == 0) {
+            for (let i = 0; i < distY; i++) {
+                this.draw(x1, y1 + i, type);
+            }
+            this.draw(x2, y2, type);
+            return;
+        }
+
+        for (let i = 0; i < distX; i++) {
+            let yPlus = Math.round(i / calc);
+            this.draw(x1 + i, y1 + yPlus, type);
+        }
+
+        this.draw(x2, y2, type);
+    }
+
+    drawRect(x1, y1, x2, y2, type = 1) {
+        this.drawLn(x1, y1, x1, y2, type);
+        this.drawLn(x1, y1, x2, y1, type);
+        this.drawLn(x1, y2, x2, y2, type);
+        this.drawLn(x2, y1, x2, y2, type);
+    }
+
+    clear() {
+        for (let i = 0; i < resulution.h; i++) {
+            for (let j = 0; j < resulution.w; j++) {
+                this.displayMap[i].push(design.bg);
+            }
+        }
+    }
+
+    render() {
+        console.clear();
+
+        let displayString = "";
+
+        this.displayMap.forEach((e) => {
+            e.forEach((e) => {
+                displayString += e;
+            });
+            displayString += "\n";
         });
-        displayString += "\n";
-    });
 
-    console.log(displayString);
-};
+        displayString += message.bottom;
+
+        console.log(displayString);
+    }
+}
+
+module.exports = Render;
